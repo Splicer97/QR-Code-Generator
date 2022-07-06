75b06a29a0b8fc8ecc9ee8f2481478c9b499c8a4
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-06 16:41:42 +0300

    add_permissions_checker

--- app/src/main/java/com/splicer/qrcodegenerator/MainActivity.kt
+++ app/src/main/java/com/splicer/qrcodegenerator/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.splicer.qrcodegenerator
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.zxing.WriterException
 
 class MainActivity : AppCompatActivity() {
@@ -22,21 +26,46 @@ class MainActivity : AppCompatActivity() {
         bGenerate = findViewById(R.id.button)
         bScanner = findViewById(R.id.bScan)
         bScanner?.setOnClickListener {
-            startActivity(Intent(this, ScannerActivity:: class.java))
+            checkCameraPermission()
         }
         bGenerate?.setOnClickListener {
             generateQrCode("Поставь лайк если понравилось видео!!")
         }
     }
 
-    private fun generateQrCode(text: String){
+    private fun generateQrCode(text: String) {
         val qrGenerator = QRGEncoder(text, null, QRGContents.Type.TEXT, 500)
         try {
             val bMap = qrGenerator.bitmap
             im?.setImageBitmap(bMap)
-        } catch (e: WriterException){
+        } catch (e: WriterException) {
 
         }
     }
 
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 12)
+        } else {
+            startActivity(Intent(this, ScannerActivity::class.java))
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 12) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivity(Intent(this, ScannerActivity::class.java))
+            }
+        }
+    }
 }
