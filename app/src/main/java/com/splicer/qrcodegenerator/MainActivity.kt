5a6362d3a9b3d81bc441e11736f57ce942a9cc54
Nikita_Kovantsov <kovancov@gmail.com>
2022-07-06 14:39:12 +0300

    donne

--- app/src/main/java/com/splicer/qrcodegenerator/MainActivity.kt
+++ app/src/main/java/com/splicer/qrcodegenerator/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     var im: ImageView? = null
     var bGenerate: Button? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
